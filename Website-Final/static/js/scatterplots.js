fea9fe0255ca5b0899611fdcccfeb774bb580feb
Steven Lee <steven_lee_1@yahoo.com>
2020-08-01 00:36:23 -0400

    Added many comments for scatterplots.js

--- Website-Final/static/js/scatterplots.js
+++ Website-Final/static/js/scatterplots.js
@@ -1,14 +1,22 @@
+// -----------------------------------------------------------------------------
+// Graph Earthquake Magnitude vs Maximum Water Height (m) using a scatter plot
+// -----------------------------------------------------------------------------
 function scatterMagnitudeVsHeight(data) {
+
+  // Define arrays to hold data for the scatter plot
   var earthquake_mag = [];
   var max_water_height = [];
 
   data.forEach(function(d) {
     if ((d["Earthquake Magnitude"] > 0) &&
         (d["Maximum Water Height (m)"] > 0)) {
+          // Add data to our arrays only if the data is > 0
           earthquake_mag.push(d["Earthquake Magnitude"]);
           max_water_height.push(d["Maximum Water Height (m)"]);
         }
   }) // end forEach
+
+  // Define the trace, data, and layout
   var trace1 = {
     x: earthquake_mag,
     y: max_water_height,
@@ -17,6 +25,7 @@ function scatterMagnitudeVsHeight(data) {
   };
     
   var plotdata = [trace1];
+
   var layout = {
     title: 'Earthquake Magnitude vs Maximum Water Height (m)',
     xaxis: {
@@ -26,22 +35,30 @@ function scatterMagnitudeVsHeight(data) {
       title: 'Maximum Water Height (m)'
     }
     };
+
+  // Create the plot
   Plotly.newPlot('scatterMagnitudeVsHeight', plotdata, layout);
-}
+} // end scatterMagnitudeVsHeight()
 
-
-
+// ---------------------------------------------------------------------------------------
+// Graph Maximum Water Height (m) vs Maximum Inundation Distance (m) using a scatter plot
+// ---------------------------------------------------------------------------------------
 function scatterHeightvsDistance(data) {
+
+  // Define arrays to hold data for the scatter plot
   var max_water_height = [];
   var max_water_dist = [];
 
   data.forEach(function(d) {
     if ((d["Maximum Water Height (m)"] > 0) &&
         (d["Max Inundation Distance (m)"] > 0)) {
+          // Add data to our arrays only if the data is > 0          
           max_water_height.push(d["Maximum Water Height (m)"]);
           max_water_dist.push(d["Max Inundation Distance (m)"]);
         }
   }) // end forEach
+
+  // Define the trace, data, and layout
   var trace1 = {
     x: max_water_height,
     y: max_water_dist,
@@ -50,6 +67,7 @@ function scatterHeightvsDistance(data) {
   };
     
   var plotdata = [trace1];
+
   var layout = {
     title: 'Maximum Water Height (m) vs Maximum Inundation Distance (m)',
     xaxis: {
@@ -59,22 +77,31 @@ function scatterHeightvsDistance(data) {
       title: 'Maximum Inundation Distance (m)'
     }
     };
+
+  // Create the plot
   Plotly.newPlot('scatterHeightvsDistance', plotdata, layout);
-}
+} // end scatterHeightvsDistance()
 
 
-
+// ---------------------------------------------------------------------------------------
+// Graph Earthquake Magnitude vs Maximum Inundation Distance (m) using a scatter plot
+// ---------------------------------------------------------------------------------------
 function scatterMagnitudevsDistance(data) {
+
+  // Define arrays to hold data for the scatter plot
   var earthquake_mag = [];
   var max_water_dist = [];
 
   data.forEach(function(d) {
     if ((d["Earthquake Magnitude"] > 0) &&
         (d["Max Inundation Distance (m)"] > 0)) {
+          // Add data to our arrays only if the data is > 0          
           earthquake_mag.push(d["Earthquake Magnitude"]);
           max_water_dist.push(d["Max Inundation Distance (m)"]);
         }
   }) // end forEach
+
+  // Define the trace, data, and layout
   var trace1 = {
     x: earthquake_mag,
     y: max_water_dist,
@@ -83,6 +110,7 @@ function scatterMagnitudevsDistance(data) {
   };
     
   var plotdata = [trace1];
+
   var layout = {
     title: 'Earthquake Magnitude vs Maximum Inundation Distance (m)',
     xaxis: {
@@ -92,8 +120,10 @@ function scatterMagnitudevsDistance(data) {
       title: 'Maximum Inundation Distance (m)'
     }
     };
+
+    // Create the plot
   Plotly.newPlot('scatterMagnitudevsDistance', plotdata, layout);
-}
+} // end scatterMagnitudevsDistance()
 
 
 
